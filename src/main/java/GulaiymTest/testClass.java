90718e5070d8121c3fc592f5493e09d0e3daface
NARGIZA001 <nkeneshbaeva1@gmail.com>
2019-12-21 17:37:05 -0600

    pojo class was created

---
+++ src/main/java/GulaiymTest/testClass.java
@@ -0,0 +1,8 @@
+package GulaiymTest;
+
+public class testClass {
+    public static void main(String[] args) {
+
+        System.out.println("this is test");
+    }
+}
---
+++ src/test/java/pojoClassByNargiza.java
@@ -0,0 +1,9 @@
+public class pojoClassByNargiza {
+
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
