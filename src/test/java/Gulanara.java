2a9dcf96d49f1ba919650aa12995bc8a46cc7ce4
zalkar2000 <aselabegmatova@gmail.com>
2019-12-22 17:20:09 -0600

    my message

--- src/test/java/Gulanara.java
+++ src/test/java/Gulanara.java
@@ -3,5 +3,6 @@ import org.codehaus.jackson.map.util.ISO8601Utils;
 public class Gulanara {
     public static void main(String[] args) {
         System.out.println("helllllooo");
+        System.out.println("hhhhh");
     }
 }
