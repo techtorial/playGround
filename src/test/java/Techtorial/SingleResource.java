06b29a015deb75ac9260c90b98aed6c506289dec
zalkar2000 <aselabegmatova@gmail.com>
2019-12-21 18:28:49 -0600

    Techtorial is best

---
+++ src/test/java/Techtorial/SingleResource.java
@@ -0,0 +1,10 @@
+package Techtorial;
+
+public class SingleResource {
+
+    public static void main(String[] args) {
+        System.out.println("Techtorial is best");
+        System.out.println("Techtorial number 1");
+    }
+
+}
---
+++ src/test/java/Techtorial/delayedResponce.java
@@ -0,0 +1,4 @@
+package Techtorial;
+
+public class delayedResponce {
+}
