db90b3fbd64f3345e23d44c4ee7eab63ac38ed32
kubatkurbanaliev <kurbanalievkubat@gmail.com>
2019-12-23 17:21:51 -0600

    Adding Package Kubat

---
+++ src/test/java/Kubat/OrderPlace.java
@@ -0,0 +1,56 @@
+package Kubat;
+
+public class OrderPlace {
+
+    private int id, petId, quantity;
+    private String shipDate, status;
+    private Boolean complete;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPetId() {
+        return petId;
+    }
+
+    public void setPetId(int petId) {
+        this.petId = petId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getShipDate() {
+        return shipDate;
+    }
+
+    public void setShipDate(String shipDate) {
+        this.shipDate = shipDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Boolean getComplete() {
+        return complete;
+    }
+
+    public void setComplete(Boolean complete) {
+        this.complete = complete;
+    }
+}
--- src/test/java/Slides.java
+++ src/test/java/Slides.java
@@ -1,11 +1,11 @@
 public class Slides {
 
-<<<<<<< HEAD
+//<<<<<<< HEAD
 
-=======
+//=======
     public static void main(String[] args) {
         System.out.println("hello world ");
     }
->>>>>>> 0b65d7d16b0a9e832f48dc22a7b06dc9fcd23945
+//>>>>>>> 0b65d7d16b0a9e832f48dc22a7b06dc9fcd23945
 
 }
