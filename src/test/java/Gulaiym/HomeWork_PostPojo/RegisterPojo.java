6803f7b02d798797005ecb5a2afd171b58437fc8
nurzhigitgulaiym <gdzhorobekova@gmail.com>
2019-12-23 21:05:59 -0600

    gulaiym homework

---
+++ src/test/java/Gulaiym/HomeWork_PostPojo/RegisterPojo.java
@@ -0,0 +1,62 @@
+package Gulaiym.HomeWork_PostPojo;
+
+    public class RegisterPojo {
+
+        private String email;
+        private String password;
+        private int id;
+        private String token;
+        private String updatedAt;
+
+        public String getUpdatedAt() {
+            return updatedAt;
+        }
+
+        public void setUpdatedAt(String updatedAt) {
+            this.updatedAt = updatedAt;
+        }
+
+        public RegisterPojo(String email, String password) {
+            this.email = email;
+            this.password = password;
+
+        }
+
+        public RegisterPojo(){
+
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getToken() {
+            return token;
+        }
+
+        public void setToken(String token) {
+            this.token = token;
+        }
+    }
+
+
