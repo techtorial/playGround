cc3a6c0fab43e00683b424f7f88241b1e18b5873
erkin1992 <ekachygulov@gmail.com>
2019-12-25 12:13:20 -0600

    BEST TESTER EVER!!!

---
+++ src/test/java/Erkin/K/RegisterPOJO.java
@@ -0,0 +1,34 @@
+package Erkin.K;
+
+public class RegisterPOJO {
+    private String email;
+    private String password;
+    private int id;
+    private String token;
+    public RegisterPOJO() {
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/test/java/Erkin/K/ReqResPUT.java
@@ -0,0 +1,27 @@
+package Erkin.K;
+
+public class ReqResPUT {
+    private String name;
+    private String job;
+    private String updatedAt;
+    public  ReqResPUT() {
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getJob() {
+        return job;
+    }
+    public void setJob(String job) {
+        this.job = job;
+    }
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
